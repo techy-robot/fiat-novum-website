ba437b49276dc877972de16e8c372adee7588ce9
techy-robot <asher.j.edwards1@gmail.com>
2026-04-30 23:51:10 -0600

    Working Keystatic integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: [
+    '@keystar/ui',
+    '@keystatic/core',
+    '@react-stately/combobox'
+  ],
   reactStrictMode: true,
 };
 
