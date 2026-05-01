161b8b31c8ff8c245239a0d0fc2769d3672d0e0b
techy-robot <asher.j.edwards1@gmail.com>
2026-05-01 11:51:26 -0600

    Fixed 404 page

---
+++ app/NotFoundClient.tsx
@@ -0,0 +1,9 @@
+// app/NotFoundClient.tsx
+"use client";
+
+// Adjust this path if your wrapper is named something else (like _404.tsx)
+import NotFoundPage from '../components/_404'; 
+
+export default function NotFoundClient() {
+  return <NotFoundPage />;
+}
