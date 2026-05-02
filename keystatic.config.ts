4c5cd76a5f572dddea603b0c36502f5f097a0bc1
techy-robot <asher.j.edwards1@gmail.com>
2026-05-02 16:09:33 -0600

    Updated dynamic content schema

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env tsx
+
 // keystatic.config.ts
 import { config, fields, collection } from '@keystatic/core';
 
@@ -25,8 +27,38 @@ export default config({
         title: fields.slug({ name: { label: 'Title' } }),
         // The date field is crucial for your URL scheme
         publishDate: fields.date({ label: 'Publish Date', validation: { isRequired: true } }),
+        cover: fields.image({ label: 'Cover Image' }),
+        summary: fields.text({ label: 'Summary', validation: { isRequired: true } }),
         content: fields.mdx({ label: 'Content' }),
       },
     }),
+    projects: collection({
+      label: 'Projects',
+      // This determines the filename (e.g., "my-awesome-post.mdx")
+      slugField: 'title', 
+      path: 'content/blog/*',
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        // The date field is crucial for your URL scheme
+        publishDate: fields.date({ label: 'Publish Date', validation: { isRequired: true } }),
+        cover: fields.image({ label: 'Cover Image' }),
+        coolnessFactor: fields.integer({ label: 'Coolness Factor', validation: {min: 0,max: 10}}),
+        summary: fields.text({ label: 'Summary', validation: { isRequired: true } }),
+        content: fields.mdx({ label: 'Content' }),
+      },
+    }),
+    skills: collection({
+      label: 'Skills',
+      slugField: 'name',
+      path: 'content/skills/*',
+      schema: {
+        name: fields.slug({ name: { label: 'Skill Name' } }),
+        proficiency: fields.integer({ label: 'Proficiency Level', validation: {min: 0,max: 10}}),
+        iconName: fields.text({ label: 'Lucide Icon Name (e.g., Cpu, Zap, Code)' }),
+        content: fields.mdx({ label: 'Write up' }),
+      },
+    }),
   },
+
+    
 });
