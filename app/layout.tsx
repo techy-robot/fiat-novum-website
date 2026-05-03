24394f7b855f84f327a5d74ad415284f0f9f085a
techy-robot <asher.j.edwards1@gmail.com>
2026-05-03 12:51:43 -0600

    Remove unused font globals (imported elsewhere)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,5 @@
 // app/layout.tsx
 import '@/styles/globals.css'
-import { Space_Mono, Handlee } from 'next/font/google';
-
-const spaceMono = Space_Mono({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-space-mono',
-});
-
-const handlee = Handlee({
-  subsets: ['latin'],
-  weight: ['400'],
-  variable: '--font-handlee',
-});
 
 export default function RootLayout({
   children,
