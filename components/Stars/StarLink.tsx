f4bd91e7e920f27af9acd49fc231992baf2b2110
techy-robot <asher.j.edwards1@gmail.com>
2026-05-27 21:54:07 -0600

    feat: adjusted star link collection radius
    Made it so you had to be over the link ready to click it

--- components/Stars/StarLink.tsx
+++ components/Stars/StarLink.tsx
@@ -6,14 +6,15 @@ import styles from "./star-game.module.css";
 export interface StarLinkProps extends Omit<React.ComponentPropsWithoutRef<typeof Link>, "href" | "children"> {
   href: string;
   children: React.ReactNode;
+  collectRadius?: number;
 }
 
-export default function StarLink({ href, children, className, ...rest }: StarLinkProps) {
+export default function StarLink({ href, children, className, collectRadius = 6, ...rest }: StarLinkProps) {
   return (
     <Link href={href} className={[styles.starLink, className ?? ""].filter(Boolean).join(" ")} {...rest}>
-      <TwinklingStar x={-14} y={-12} size={11} twinkleDuration={2.2} twinkleDelay={0.1} />
-      <TwinklingStar x={66} y={-18} size={10} twinkleDuration={2.7} twinkleDelay={0.55} />
-      <TwinklingStar x={28} y={22} size={12} twinkleDuration={2.4} twinkleDelay={0.9} />
+      <TwinklingStar x={-14} y={-12} size={11} twinkleDuration={2.2} twinkleDelay={0.1} collectRadius={collectRadius} />
+      <TwinklingStar x={66} y={-18} size={10} twinkleDuration={2.7} twinkleDelay={0.55} collectRadius={collectRadius} />
+      <TwinklingStar x={28} y={22} size={12} twinkleDuration={2.4} twinkleDelay={0.9} collectRadius={collectRadius} />
       <span className={styles.starLinkLabel}>{children}</span>
     </Link>
   );
