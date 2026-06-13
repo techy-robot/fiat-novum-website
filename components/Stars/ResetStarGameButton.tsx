42cf2fe9e16ae27b976bddef53fe2cd1e3fc92b0
techy-robot <asher.j.edwards1@gmail.com>
2026-06-12 22:48:02 -0600

    feat: add text prop for rst button
    Very basic customization right now. Eventually I want a generic button component that has any text, and any code trigger

--- components/Stars/ResetStarGameButton.tsx
+++ components/Stars/ResetStarGameButton.tsx
@@ -5,9 +5,11 @@ import { starGame } from "@/lib/starGame";
 import projectcss from "@/components/plasmic/fiat_novum/plasmic.module.css";
 import styles from "./ResetStarGameButton.module.css";
 
-export interface ResetStarGameButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+export interface ResetStarGameButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  text: string
+}
 
-export default function ResetStarGameButton({ className, type = "button", onClick, ...rest }: ResetStarGameButtonProps) {
+export default function ResetStarGameButton({ className, type = "button", onClick, text = "Reset Star Collection", ...rest }: ResetStarGameButtonProps) {
   const handleClick = React.useCallback(
     (event: React.MouseEvent<HTMLButtonElement>) => {
       onClick?.(event);
@@ -21,7 +23,7 @@ export default function ResetStarGameButton({ className, type = "button", onClic
 
   return (
     <button type={type} className={[styles.resetButton, projectcss.plasmic_default_styles, projectcss.plasmic_tokens, className ?? ""].filter(Boolean).join(" ")} onClick={handleClick} {...rest}>
-      Reset Star Collection
+      {text}
     </button>
   );
 }
